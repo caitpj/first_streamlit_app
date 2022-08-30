d50bb3f570e4997ea17d13cfbe8d472185095011
Cai Parry-Jones <97813242+caitpj@users.noreply.github.com>
2022-08-30 21:29:11 +0100

    Update streamlit_app.py
    fruitvice set up

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selector]
 # load csv from s3 and display on streamlit app
 streamlit.dataframe(fruits_to_show)
 
-# Display the table on the page.
+# New Section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
